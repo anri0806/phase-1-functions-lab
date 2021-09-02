2a713efcc4a36c105fb08312b0e695b776b501f8
Anri <anri0806@hotmail.co.jp>
2021-09-02 17:42:50 -0400

    commit tree 6ec09e7fd3896d38dedbee412157ac2925318baa

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) { 
+    if (pickupLocation >= 42){
+    return pickupLocation - 42;
+    } else {
+        return 42 - pickupLocation;
+    }
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    if (pickupLocation >= 42){
+        return distanceFromHqInBlocks(pickupLocation) * 264
+    } else {
+        return distanceFromHqInBlocks(pickupLocation) * 264
+    }
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination){
+        return (destination - start) * 264;
+    }else {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance < 400) {
+        return 0;
+    } else if (distance > 400 && distance < 2000) {
+        return  (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else if(distance > 2500) {
+        return 'cannot travel that far';
+    } 
+}
